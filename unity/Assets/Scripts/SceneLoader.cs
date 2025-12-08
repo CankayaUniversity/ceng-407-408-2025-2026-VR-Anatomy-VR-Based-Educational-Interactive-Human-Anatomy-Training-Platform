7c19c6f8c549818b4f5b4f63afff04ec24090e75
NazT <naztls08@gmail.com>
2025-12-08 13:52:50 +0300

    feat(vr): add module scenes and XR device simulator config for main menu

---
+++ unity/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Ana menü
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("01_Menu");
+    }
+
+    // Hareket Sistemi VR sahnesi
+    public void LoadMotionSystem()
+    {
+        SceneManager.LoadScene("02_MotionSystem");
+    }
+
+    // Dolaşım Sistemi VR sahnesi
+    public void LoadCirculationSystem()
+    {
+        SceneManager.LoadScene("03_CirculationSystem");
+    }
+
+    // Quiz sahnesi
+    public void LoadQuiz()
+    {
+        SceneManager.LoadScene("04_Quiz");
+    }
+
+    // Yapay Zeka sahnesi
+    public void LoadAIChat()
+    {
+        SceneManager.LoadScene("05_AIChat");
+    }
+
+    // Hakkında
+    public void LoadAbout()
+    {
+        SceneManager.LoadScene("06_About");
+    }
+
+// Hakkında
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("07_Settings");
+    }
+
+    // Uygulamadan çıkmak için (PC build’inde)
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+        Application.Quit();
+    }
+}
